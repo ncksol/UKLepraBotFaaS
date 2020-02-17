c6526688a95675ed0562e9a3b99011d2b7454ffe
Nick Sologoub <nick.sologoub@outlook.com>
2020-02-17 15:06:53 +0000

    fixed huify not respecting delay settings

--- UKLepraBotFaaS/Functions/HuifyFunction.cs
+++ UKLepraBotFaaS/Functions/HuifyFunction.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using Telegram.Bot.Types;
 using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace UKLepraBotFaaS.Functions
 {
@@ -16,7 +17,9 @@ namespace UKLepraBotFaaS.Functions
 
         [FunctionName("HuifyFunction")]
         public async static Task Run(
-            [QueueTrigger(Constants.HuifyQueueName)]Message input, 
+            [QueueTrigger(Constants.HuifyQueueName)]Message input,
+            [Blob(Constants.ChatSettingsBlobPath)] string chatSettingsString,
+            [Blob(Constants.DataBlobPath)] CloudBlobContainer chatSettingsOutput,
             [Queue(Constants.OutputQueueName)] CloudQueue output, 
             ILogger log)
         {
@@ -24,6 +27,15 @@ namespace UKLepraBotFaaS.Functions
 
             try
             {
+                var chatSettings = JsonConvert.DeserializeObject<ChatSettings>(chatSettingsString);
+
+                var shouldProcessMessage = ShouldProcessMessage(chatSettings, input);
+
+                var settingsBlob = chatSettingsOutput.GetBlockBlobReference("chatsettings.json");
+                await settingsBlob.UploadTextAsync(JsonConvert.SerializeObject(chatSettings));
+
+                if(shouldProcessMessage == false) return;
+
                 var message = input.Text;
                 var huifiedMessage = HuifyMeInternal(message);
 
@@ -34,6 +46,37 @@ namespace UKLepraBotFaaS.Functions
             {
                 log.LogError(e, "Error while huifying message");
             }
+        }
+
+        private static bool ShouldProcessMessage(ChatSettings chatSettings, Message message)
+        {            
+            var conversationId = message.Chat.Id.ToString();
+
+            var state = chatSettings.State;
+            var delay = chatSettings.Delay;
+            var delaySettings = chatSettings.DelaySettings;
+            if (!state.ContainsKey(conversationId) || !state[conversationId])//huify is not active or was never activated
+                return false;
+
+            if (delay.ContainsKey(conversationId))
+            {
+                if(delay[conversationId] > 0)
+                    delay[conversationId] -= 1;
+            }
+            else
+            {
+                Tuple<int, int> delaySetting;
+                if (delaySettings.TryGetValue(conversationId, out delaySetting))
+                    delay[conversationId] = _rnd.Next(delaySetting.Item1, delaySetting.Item2 + 1);
+                else
+                    delay[conversationId] = _rnd.Next(4);
+            }
+
+            if (delay[conversationId] != 0 || message.From.Id.ToString() == Configuration.Instance.MasterId) return false;
+
+            delay.Remove(conversationId);
+
+            return true;
         }
 
         private static string HuifyMeInternal(string message)
